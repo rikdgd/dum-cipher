f061872c58ea9070d51fc280785d4c4b7dd65138
rikdgd <74911706+rikdgd@users.noreply.github.com>
2024-12-04 11:06:55 +0100

    rename variables in generate_hmac function.

--- src/authentication.rs
+++ src/authentication.rs
@@ -17,13 +17,13 @@ type HmacSha256 = Hmac<Sha256>;
 pub fn generate_hmac(data: &[u8], key: &[u8]) -> Option<[u8; 32]> {
     let mut mac = HmacSha256::new_from_slice(key).ok()?;
     mac.update(data);
-    let res = mac.finalize();
+    let res_mac = mac.finalize();
 
-    let code_bytes = res.into_bytes().to_vec();
+    let mac_bytes = res_mac.into_bytes().to_vec();
     
-    if code_bytes.len() == 32 {
+    if mac_bytes.len() == 32 {
         let mut mac_arr = [0u8; 32];
-        for (i, byte) in code_bytes.iter().enumerate() {
+        for (i, byte) in mac_bytes.iter().enumerate() {
             mac_arr[i] = byte.clone();
         }
         Some(mac_arr)
