b17a33ea1d977e3fa0318296f2dfcdf9e0641bd7
rikdgd <74911706+rikdgd@users.noreply.github.com>
2024-12-14 16:08:45 +0100

    Create simple testing function to make debugging easier

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,53 @@ mod dum_file_encryptor;
 mod authentication;
 
 use std::env;
+use std::fs::OpenOptions;
+use std::io::ErrorKind;
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() != 4 {
+    //     return Err(std::io::Error::new(
+    //         std::io::ErrorKind::InvalidInput,
+    //         "Invalid arguments provided"
+    //     ));
+    // }
+    // 
+    // let action = String::from(&args[1]);
+    // let action = action.as_str();
+    // let file_path = Path::new(&args[2]);
+    // let password = String::from(&args[3]);
+    // 
+    // let encryptor = dum_file_encryptor::DumFileEncryptor::new(file_path)?;
+    // match action {
+    //     "encrypt" => encryptor.encrypt_file(&password)?,
+    //     "decrypt" => encryptor.decrypt_file(&password)?,
+    //     _ => panic!("Could not understand requested action"),
+    // }
+    // 
+    // Ok(())
+    
+    encryption_test()
+}
+
+
+/// Encrypts and then decrypts a default file. Makes it easier to debug.
+#[allow(unused)]
+fn encryption_test() -> std::io::Result<()> {
+    let file_path = Path::new("./test-file.txt");
+    if !file_path.is_file() {
         return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "Invalid arguments provided"
+            ErrorKind::NotFound, 
+            "Could not find the testing file."
         ));
     }
-    
-    let action = String::from(&args[1]);
-    let action = action.as_str();
-    let file_path = Path::new(&args[2]);
-    let password = String::from(&args[3]);
+    let password = "welcome123";
     
     let encryptor = dum_file_encryptor::DumFileEncryptor::new(file_path)?;
-    match action {
-        "encrypt" => encryptor.encrypt_file(&password)?,
-        "decrypt" => encryptor.decrypt_file(&password)?,
-        _ => panic!("Could not understand requested action"),
-    }
+
+    encryptor.encrypt_file(password)?;
+    encryptor.decrypt_file(password)?;
     
     Ok(())
 }
